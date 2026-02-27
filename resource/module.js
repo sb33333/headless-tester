d17561e169507f7ba37a9dd54b31557aa736119d
sb33333 <97681301+sb33333@users.noreply.github.com>
2026-02-28 08:40:53 +0900

    Fix import path for EventHandlers module

--- resource/module.js
+++ resource/module.js
@@ -1,4 +1,4 @@
 export * as Promises from "./src/promises.js";
 export * as JobQueues from "./src/job-queue.js";
 export {default as HeadlessTester} from "./src/headless-tester.js";
-export * as EventHandlers from "./src/event-handlers.js";
+export * as EventHandlers from "./src/event-handler.js";
